691d07ac251c71f97c863ecdbb0fc5eed692578a
amazingefren <efrentheroker@hotmail.com>
2021-08-04 19:43:16 -0600

    localstorage -> access+refresh token

--- src/auth/auth.store.ts
+++ src/auth/auth.store.ts
@@ -1,11 +1,17 @@
 const TOKEN_KEY = 'token';
+const REFRESH_TOKEN_KEY = "refresh_token"
 
-export const saveAuthData = ({token}) => {
+export const saveAuthData = ({token, refresh_token}: any) => {
   localStorage.setItem(TOKEN_KEY, token)
+  localStorage.setItem(REFRESH_TOKEN_KEY, refresh_token)
 }
 
 export const getToken = () => {
   return localStorage.getItem(TOKEN_KEY)
+}
+
+export const getRefreshToken = () => {
+  return localStorage.getItem(REFRESH_TOKEN_KEY)
 }
 
 export const clearStorage = () => {
